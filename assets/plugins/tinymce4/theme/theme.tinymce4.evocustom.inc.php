97a598582fc347c9ce1357e9989e84fb4eb0559a
Marc <marc@graffx.nl>
2021-03-16 14:14:31 +0100

    Rename file and removed menu and statusbar

--- assets/plugins/tinymce4/theme/theme.tinymce4.evocustom.inc.php
+++ assets/plugins/tinymce4/theme/theme.tinymce4.evocustom.inc.php
@@ -12,10 +12,14 @@
  * $emptyAllowed    = true, false (allows param:'' instead of falling back to default)
  * If $editorParam is empty and $emptyAllowed is true, $defaultValue will be ignored
  *
- * $this->modxParams holds an array of actual Modx- / user-settings
+ * $this->modxParams holds an array of actual EVO / user-settings
  *
  * */
 
 $this->set('plugins', 'anchor advlist autolink lists modxlink image charmap print preview hr anchor pagebreak searchreplace wordcount visualblocks visualchars code fullscreen spellchecker insertdatetime media nonbreaking save table contextmenu directionality emoticons paste textcolor codesample colorpicker textpattern imagetools paste youtube', 'string');
 $this->set('toolbar1', 'undo redo | bold italic underline strikethrough | copy paste | searchreplace | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent', 'string');
 $this->set('toolbar2', 'styleselect formatselect link unlink image media table | hr removeformat | charmap | nonbreaking | preview fullscreen code', 'string');
+
+// Hide bars
+$this->set('menubar', false, 'bool' );       // https://www.tinymce.com/docs/configure/editor-appearance/#menubar
+$this->set('statusbar', false, 'bool' );       // https://www.tinymce.com/docs/get-started/customize-ui/#hidingthestatusbar
